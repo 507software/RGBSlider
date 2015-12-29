de445fc27588020c18b58ce028340923c9cadc54
Ricardo Rodriguez <ricarod@ricardos-macbook-pro.local>
2015-12-29 11:16:09 -0500

    squareColor added

--- RGBSliders/ViewController.swift
+++ RGBSliders/ViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sliderRED: UISlider!
+    @IBOutlet weak var sliderGREEN: UISlider!
+    @IBOutlet weak var sliderBLUE: UISlider!
+    
+//    @IBOutlet weak var colorSquare: UIView!
+    
+    
+    @IBOutlet weak var colorSquare: UIView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateBGColor()
+        colorSquare.layer.backgroundColor = UIColor.blackColor().CGColor
+        colorSquare.layer.borderWidth = 1
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +32,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func updateBGColor() {
+        let red = CGFloat(sliderRED.value)
+        let green = CGFloat(sliderGREEN.value)
+        let blue = CGFloat(sliderBLUE.value)
+        
+        colorSquare.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+    }
 }
 
